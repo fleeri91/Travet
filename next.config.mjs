6047697aec04c4894917f0de2a23f2ffcb51ed3a
Flemming Eriksson <fleeri91@gmail.com>
2024-03-20 18:51:51 +0100

    proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/day/:path*',
+        destination: 'https://www.atg.se/services/racinginfo/v1/api/calendar/day/:path*',
+      },
+      {
+        source: '/api/game/:path*',
+        destination: 'https://www.atg.se/services/racinginfo/v1/api/games/:path*',
+      },
+      {
+        source: '/api/race/:path*',
+        destination: 'https://www.atg.se/services/racinginfo/v1/api/races/:path*',
+      },
+      {
+        source: '/api/race/:path*/start/:path*',
+        destination: 'https://www.atg.se/services/racinginfo/v1/api/races/:path*/start/:path*',
+      },
+    ]
+  },
+}
 
 export default nextConfig

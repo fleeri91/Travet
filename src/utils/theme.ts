db71c1cc11c47c3dbc7afcb587fb6a6aaa3244eb
Flemming <fleeri91@gmail.com>
2024-03-22 16:01:01 +0100

    colors

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,34 @@
+import { Color } from '@tremor/react'
+
+import { GameType } from '@/constants/GameType'
+
 export const applyThemePreference = (darkMode: boolean) => {
   const root = window.document.documentElement
   root.classList.remove(darkMode ? 'light' : 'dark')
   root.classList.add(darkMode ? 'dark' : 'light')
+}
+
+export const gameTypeColor = (gameType: GameType): Color => {
+  switch (gameType) {
+    case 'v75':
+      return 'blue'
+    case 'gs75':
+      return 'emerald'
+    case 'v86':
+      return 'purple'
+    case 'v64':
+      return 'orange'
+    case 'v65':
+      return 'red'
+    case 'dd':
+      return 'sky'
+    case 'v5':
+      return 'sky'
+    case 'v4':
+      return 'sky'
+    case 'v3':
+      return 'sky'
+    default:
+      return 'neutral'
+  }
 }

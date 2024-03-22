9c716529b37ce2ae68b35d24ccff7f941fdd74ff
Flemming <fleeri91@gmail.com>
2024-03-22 16:01:39 +0100

    date nav

--- src/store/useCalendar.ts
+++ src/store/useCalendar.ts
@@ -10,20 +10,22 @@ type CalendarState = {
 }
 
 type CalendarActions = {
-  setSelectedDate: (date: string) => void
+  setPreviousDate: (date: string) => void
+  setNextDate: (date: string) => void
   setCalendarData: (data: CalendarDayRoot) => void
-  resetSelectedDate: () => void
 }
 
 const initialState: CalendarState = {
-  today: dayjs().add(0, 'day').format('YYYY-MM-DD'),
+  today: dayjs().format('YYYY-MM-DD'),
   selectedDate: dayjs().format('YYYY-MM-DD'),
   calendarData: {} as CalendarDayRoot,
 }
 
 export const useCalendarStore = create<CalendarState & CalendarActions>((set) => ({
   ...initialState,
-  setSelectedDate: (newDate: string) => set((state) => ({ ...state, selectedDate: newDate })),
+  setPreviousDate: (date: string) =>
+    set((state) => ({ ...state, selectedDate: dayjs(date).subtract(1, 'day').format('YYYY-MM-DD') })),
+  setNextDate: (date: string) =>
+    set((state) => ({ ...state, selectedDate: dayjs(date).add(1, 'day').format('YYYY-MM-DD') })),
   setCalendarData: (newCalendarData: CalendarDayRoot) => set((state) => ({ ...state, calendarData: newCalendarData })),
-  resetSelectedDate: () => set((state) => ({ ...state, selectedDate: initialState.selectedDate })),
 }))

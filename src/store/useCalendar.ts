367c087dbca40d11f60687b9b7ece92e59b9876e
Flemming <fleeri91@gmail.com>
2024-03-22 17:26:58 +0100

    loading state

--- src/store/useCalendar.ts
+++ src/store/useCalendar.ts
@@ -7,18 +7,21 @@ type CalendarState = {
   today: string
   selectedDate: string
   calendarData: CalendarDayRoot
+  isLoading: boolean
 }
 
 type CalendarActions = {
   setPreviousDate: (date: string) => void
   setNextDate: (date: string) => void
   setCalendarData: (data: CalendarDayRoot) => void
+  setIsLoading: (value: boolean) => void
 }
 
 const initialState: CalendarState = {
   today: dayjs().format('YYYY-MM-DD'),
   selectedDate: dayjs().format('YYYY-MM-DD'),
   calendarData: {} as CalendarDayRoot,
+  isLoading: false,
 }
 
 export const useCalendarStore = create<CalendarState & CalendarActions>((set) => ({
@@ -27,5 +30,7 @@ export const useCalendarStore = create<CalendarState & CalendarActions>((set) =>
     set((state) => ({ ...state, selectedDate: dayjs(date).subtract(1, 'day').format('YYYY-MM-DD') })),
   setNextDate: (date: string) =>
     set((state) => ({ ...state, selectedDate: dayjs(date).add(1, 'day').format('YYYY-MM-DD') })),
-  setCalendarData: (newCalendarData: CalendarDayRoot) => set((state) => ({ ...state, calendarData: newCalendarData })),
+  setCalendarData: (data: CalendarDayRoot) => set((state) => ({ ...state, calendarData: data })),
+
+  setIsLoading: (value: boolean) => set((state) => ({ ...state, isLoading: value })),
 }))

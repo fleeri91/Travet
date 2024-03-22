1d375d608dbfb2b34a996f0337cac11552e67c16
Flemming <fleeri91@gmail.com>
2024-03-22 16:01:09 +0100

    atg utils

---
+++ src/utils/atg.ts
@@ -0,0 +1,36 @@
+export const _getHorseSex = (sex: string): string => {
+  switch (sex) {
+    case 'mare':
+      return 's'
+    case 'gelding':
+      return 'v'
+    case 'stallion':
+      return 'h'
+    default:
+      return ''
+  }
+}
+
+export const _getGameStatus = (status: string): string => {
+  switch (status) {
+    case 'bettable':
+      return 'Spelbar'
+    case 'results':
+      return 'Avslutad'
+    default:
+      return ''
+  }
+}
+
+export const _getTrackCondition = (condition: string) => {
+  switch (condition) {
+    case 'light':
+      return 'Lätt'
+    case 'dead':
+      return 'Tung'
+    case 'winter':
+      return 'Vinter'
+    default:
+      return ''
+  }
+}

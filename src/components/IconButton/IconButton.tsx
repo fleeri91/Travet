b6b2d32f14928ac70bddd71df13dda7773cfc517
Flemming <fleeri91@gmail.com>
2024-03-22 16:02:23 +0100

    Icon button

---
+++ src/components/IconButton/IconButton.tsx
@@ -0,0 +1,17 @@
+import { Button, ButtonProps } from '@tremor/react'
+
+const IconButton = ({ children, onClick, className, ...htmlAttributes }: ButtonProps): JSX.Element | null => {
+  return (
+    <button
+      onClick={onClick}
+      className={
+        'cursor-pointer rounded-full transition-colors hover:bg-slate-400/10 focus:outline-none focus:ring-2 focus:ring-slate-300/50'
+      }
+      {...htmlAttributes}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default IconButton

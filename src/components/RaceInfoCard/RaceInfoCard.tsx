83b026608f56195db859eebc967d18e585c2eedd
Flemming <fleeri91@gmail.com>
2024-03-23 12:00:54 +0100

    style fix

--- src/components/RaceInfoCard/RaceInfoCard.tsx
+++ src/components/RaceInfoCard/RaceInfoCard.tsx
@@ -11,20 +11,24 @@ interface RaceInfoCardProps {
   race: Race | null
 }
 
-const RaceInfoCard = ({ race }: RaceInfoCardProps) => (
-  <Card className={clsx('mb-2 bg-theme-600 text-white')}>
-    {race && (
-      <Flex justifyContent="start" className="space-x-4">
-        <Text>{race.track.name}</Text>
-        <Text>{race.distance + 'm'}</Text>
-        <Text>{race.startMethod}</Text>
+const RaceInfoCard = ({ race }: RaceInfoCardProps) => {
+  return (
+    <Card className={clsx('mb-2 select-none bg-theme-600 text-white')}>
+      {race && (
+        <Flex justifyContent="start" className="space-x-2 ">
+          <Text className="text-white">{race.track.name}</Text>
+          <Text className="text-white">{race.distance + 'm'}</Text>
+          <Text className="text-white">{race.startMethod}</Text>
+        </Flex>
+      )}
+      <Flex>
+        {race && race.track.condition && (
+          <Text className="text-white">{`Banförhållande: ${_getTrackCondition(race.track.condition)}`}</Text>
+        )}
       </Flex>
-    )}
-    <Flex>
-      {race && <Text>{`Banförhållande: ${_getTrackCondition(race.track.condition)}`}</Text>}
-    </Flex>
-    <Flex>{race && <Text>{race.name}</Text>}</Flex>
-  </Card>
-)
+      <Flex>{race && <Text className="text-white">{race.name}</Text>}</Flex>
+    </Card>
+  )
+}
 
 export default RaceInfoCard

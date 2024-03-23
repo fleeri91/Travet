13514b345f2ad1aeaf5017f0939468e99fccaea4
Flemming <fleeri91@gmail.com>
2024-03-23 12:02:06 +0100

    game with records

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -1,38 +1,43 @@
-import { GameRoot } from '@/types/ATG/Game'
-import { RecordRoot } from '@/types/ATG/Record'
+import { GameRoot, Start } from '@/types/ATG/Game' // Import Start interface
+import { RecordRoot } from '@/types/ATG/Record' // Import RecordRoot interface
 import axios from 'axios'
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const params = request.nextUrl.searchParams
-  let date = params.get('date')
+  let id = params.get('id')
 
   try {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/games/${date}`)
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/games/${id}`)
 
     if (!response) {
       throw new Error('Failed to fetch game data')
     }
 
-    let data: RecordRoot[][] = []
+    let data: GameRoot | null = null
 
     if (response.data) {
       const gameData: GameRoot = response.data as GameRoot
       const raceData = await Promise.all(
         gameData.races.map(async (race) => {
-          const startRequests = race.starts.map((start) => {
-            return axios.get(
+          const startRequests = race.starts.map(async (start) => {
+            const startResponse = await axios.get<RecordRoot>(
               `${process.env.NEXT_PUBLIC_API_URL}/races/${race.id}/start/${start.number}`
             )
+            const startWithData: Start = {
+              ...start,
+              records: startResponse.data.horse.results.records,
+            }
+            return startWithData
           })
 
-          const startResponses = await Promise.all(startRequests)
+          const starts = await Promise.all(startRequests)
 
-          return startResponses.map((response) => response.data)
+          return { ...race, starts }
         })
       )
 
-      data = raceData
+      data = { ...gameData, races: raceData }
     }
 
     return NextResponse.json(data)

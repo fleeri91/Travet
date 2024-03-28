65856f7b7925ee8515ae6e0f007f3aabee36b733
Flemming <fleeri91@gmail.com>
2024-03-28 16:29:50 +0100

    spacing fix

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -8,7 +8,7 @@ const ButtonGroup = ({
   ...htmlAttributes
 }: ButtonGroupProps): JSX.Element | null => (
   <div
-    className="group fixed bottom-0 right-0 flex flex-col gap-2 xl:relative xl:p-4"
+    className="group fixed bottom-0 right-0 flex flex-col gap-2 p-4 xl:relative xl:p-0"
     {...htmlAttributes}
   >
     {children}

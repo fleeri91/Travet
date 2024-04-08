b7f9ed07b0f6b17fb81b26efe54437236e69191c
Flemming <fleeri91@gmail.com>
2024-04-08 22:44:17 +0200

    text grid

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
-import { Card, Title, Subtitle, Flex } from '@tremor/react'
+import { Card, Subtitle, Flex, Grid } from '@tremor/react'
 
 import { gameTypeColor } from '@/utils/theme'
 import { GameType } from '@/constants/GameType'
@@ -33,17 +33,25 @@ const GameCard = ({
       onClick={onClick}
     >
       <Flex>
-        <Subtitle className="uppercase text-white">{gameType}</Subtitle>
-        <Subtitle className="absolute left-1/2 -translate-x-1/2 capitalize text-white">
-          {calendarData.tracks
-            .filter((track) => tracks?.includes(track.id))
-            .map((track) => `${track.name} `)}
-        </Subtitle>
-        <Subtitle className="text-white">
-          {status && (status == GameStatus.ongoing || status == GameStatus.results)
-            ? _getGameStatus(status)
-            : dayjs(time).format('HH:mm')}
-        </Subtitle>
+        <Grid numItems={3} className="w-full">
+          <Grid>
+            <Subtitle className="uppercase text-white">{gameType}</Subtitle>
+          </Grid>
+          <Grid>
+            <Subtitle className="text-center capitalize text-white">
+              {calendarData.tracks
+                .filter((track) => tracks?.includes(track.id))
+                .map((track) => `${track.name} `)}
+            </Subtitle>
+          </Grid>
+          <Grid>
+            <Subtitle className="text-right text-white">
+              {status && (status == GameStatus.ongoing || status == GameStatus.results)
+                ? _getGameStatus(status)
+                : dayjs(time).format('HH:mm')}
+            </Subtitle>
+          </Grid>
+        </Grid>
       </Flex>
     </Card>
   )

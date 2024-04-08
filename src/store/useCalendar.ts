0a3804493c3aa15144a5da8c0d432e6b7c59f92e
Flemming <fleeri91@gmail.com>
2024-04-08 22:29:29 +0200

    removed unused prop

--- src/store/useCalendar.ts
+++ src/store/useCalendar.ts
@@ -7,7 +7,6 @@ type CalendarState = {
   today: string
   selectedDate: string
   calendarData: CalendarDayRoot
-  biggestGame: string
   isLoading: boolean
   games: [string, Game[]][]
 }
@@ -16,7 +15,6 @@ type CalendarActions = {
   setPreviousDate: (date: string) => void
   setNextDate: (date: string) => void
   setCalendarData: (data: CalendarDayRoot) => void
-  setBiggestGame: (value: string) => void
   setIsLoading: (value: boolean) => void
   setGames: (value: [string, Game[]][]) => void
 }
@@ -25,7 +23,6 @@ const initialState: CalendarState = {
   today: dayjs().format('YYYY-MM-DD'),
   selectedDate: dayjs().format('YYYY-MM-DD'),
   calendarData: {} as CalendarDayRoot,
-  biggestGame: '',
   isLoading: false,
   games: [],
 }
@@ -40,7 +37,6 @@ export const useCalendarStore = create<CalendarState & CalendarActions>((set) =>
   setNextDate: (date: string) =>
     set((state) => ({ ...state, selectedDate: dayjs(date).add(1, 'day').format('YYYY-MM-DD') })),
   setCalendarData: (data: CalendarDayRoot) => set((state) => ({ ...state, calendarData: data })),
-  setBiggestGame: (value: string) => set((state) => ({ ...state, biggestGame: value })),
   setIsLoading: (value: boolean) => set((state) => ({ ...state, isLoading: value })),
   setGames: (value: [string, Game[]][]) => set((state) => ({ ...state, games: value })),
 }))

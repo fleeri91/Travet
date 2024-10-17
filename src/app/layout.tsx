edb023a7403837bc6a25006539476378e60fb944
Flemming <fleeri91@gmail.com>
2024-10-17 18:40:38 +0200

    font fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
 import type { Metadata } from 'next'
-import { Titillium_Web } from 'next/font/google'
+import { Noto_Sans, Source_Code_Pro } from 'next/font/google'
 import './globals.css'
 
 import { SWRProvider } from '@/providers/SWRProvider'
 import { ThemeProvider } from '@/context/themeContext'
 
-const TitilliumWeb = Titillium_Web({
+const SourceCodePro = Source_Code_Pro({
   subsets: ['latin'],
-  weight: ['200', '300', '400', '600', '700', '900'],
+  weight: ['200', '300', '400', '600', '700', '800', '900'],
   variable: '--font-source-code-pro',
+})
+
+const NotoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '600', '700', '800', '900'],
+  variable: '--font-noto-sans',
 })
 
 export const metadata: Metadata = {
@@ -25,7 +31,7 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
-      <body className={`${TitilliumWeb.className}`}>
+      <body className={`${NotoSans.className} ${SourceCodePro.className}`}>
         <main className="flex justify-center ">
           <ThemeProvider>
             <SWRProvider>{children}</SWRProvider>

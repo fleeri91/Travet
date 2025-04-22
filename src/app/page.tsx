76dd1d1b07ba02b0f9ed6b5e9c32e770f0c49903
Flemming <fleeri91@gmail.com>
2025-04-22 16:06:46 +0200

    error and loading handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { Flex } from '@tremor/react'
+import { Flex, Text, Button } from '@tremor/react'
 import useSWR from 'swr'
 
 import GameSelector from '@/components/GameSelector'
 import RaceTab from '@/components/RaceTab'
 import Filter from '@/components/Filter'
+import SkeletonLoading from '@/components/ui/SkeletonLoading'
 
 import { useGameStore } from '@/store/useGame'
 import { useModalsStore } from '@/store/useModals'
@@ -14,13 +15,43 @@ import { GameRoot } from '@/types/ATG/Game'
 
 const Home = () => {
   const { gameId } = useGameStore()
-
   const { filterOpen, setFilterOpen } = useModalsStore()
 
-  const { data } = useSWR<GameRoot>(gameId ? `game/?id=${gameId}` : null, {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  })
+  const { data, error, isLoading, mutate } = useSWR<GameRoot>(
+    gameId ? `game/?id=${gameId}` : null,
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      errorRetryCount: 3,
+      errorRetryInterval: 5000,
+    }
+  )
+
+  if (isLoading) {
+    return (
+      <Flex flexDirection="row" className="my-4 max-w-screen-lg px-2 sm:my-8 sm:px-4 lg:my-16">
+        <SkeletonLoading />
+      </Flex>
+    )
+  }
+
+  if (error) {
+    return (
+      <Flex
+        flexDirection="col"
+        justifyContent="center"
+        alignItems="center"
+        className="h-screen max-w-screen-lg px-2"
+      >
+        <p className="mb-8 text-xl leading-6 text-tremor-content dark:text-dark-tremor-content">
+          Misslyckades med att ladda data
+        </p>
+        <Button onClick={() => mutate()} color="blue" variant="primary">
+          Försök igen
+        </Button>
+      </Flex>
+    )
+  }
 
   return (
     <Flex flexDirection="row" className="my-4 max-w-screen-lg px-2 sm:my-8 sm:px-4 lg:my-16">

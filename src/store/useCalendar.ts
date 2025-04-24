5bf7ab33fffb7f05d05b393f34751e20b7d89b77
Flemming Eriksson <fleeri91@gmail.com>
2025-04-25 00:23:36 +0200

    removed unused stores

--- src/store/useCalendar.ts
+++ src/store/useCalendar.ts
@@ -1,31 +1,19 @@
 import { create } from 'zustand'
 import dayjs from 'dayjs'
 
-import { CalendarDayRoot, Game } from '@/types/ATG/CalendarDay'
-
 type CalendarState = {
   today: string
   selectedDate: string
-  calendarData: CalendarDayRoot
-  isLoading: boolean
-  games: [string, Game[]][]
 }
 
 type CalendarActions = {
   setPreviousDate: (date: string) => void
   setNextDate: (date: string) => void
-  setDate: (date: string) => void
-  setCalendarData: (data: CalendarDayRoot) => void
-  setIsLoading: (value: boolean) => void
-  setGames: (value: [string, Game[]][]) => void
 }
 
 const initialState: CalendarState = {
   today: dayjs().format('YYYY-MM-DD'),
   selectedDate: dayjs().format('YYYY-MM-DD'),
-  calendarData: {} as CalendarDayRoot,
-  isLoading: false,
-  games: [],
 }
 
 export const useCalendarStore = create<CalendarState & CalendarActions>((set) => ({
@@ -37,9 +25,4 @@ export const useCalendarStore = create<CalendarState & CalendarActions>((set) =>
     })),
   setNextDate: (date: string) =>
     set((state) => ({ ...state, selectedDate: dayjs(date).add(1, 'day').format('YYYY-MM-DD') })),
-  setDate: (date: string) =>
-    set((state) => ({ ...state, selectedDate: dayjs(date).format('YYYY-MM-DD') })),
-  setCalendarData: (data: CalendarDayRoot) => set((state) => ({ ...state, calendarData: data })),
-  setIsLoading: (value: boolean) => set((state) => ({ ...state, isLoading: value })),
-  setGames: (value: [string, Game[]][]) => set((state) => ({ ...state, games: value })),
 }))

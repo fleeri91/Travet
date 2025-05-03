81af7434586777ce6f53cc5cf4010d98bdec02cc
Flemming <fleeri91@gmail.com>
2025-05-03 23:33:58 +0200

    segment select

--- src/components/RaceTab/RaceTab.tsx
+++ src/components/RaceTab/RaceTab.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { Box, Flex, IconButton, Tabs } from '@radix-ui/themes'
+import { Box, Flex, IconButton, SegmentedControl, Tabs } from '@radix-ui/themes'
 import { RiFilter3Line, RiCalendarLine } from '@remixicon/react'
 
 import RaceFilterTable from '@/components/RaceFilterTable'
+import RaceStatistics from '@/components/RaceStatistics'
+import H2H from '@/components/H2H'
 import RaceInfoCard from '@/components/ui/RaceInfoCard'
 import Filter from '@/components/Filter'
 
@@ -16,6 +18,7 @@ interface RaceTabProps {
 const RaceTab = ({ gameData }: RaceTabProps) => {
   const router = useRouter()
   const [filterOpen, setFilterOpen] = useState<boolean>(false)
+  const [view, setView] = useState<'start' | 'statistics' | 'h2h'>('start')
 
   if (!gameData) {
     return null
@@ -30,19 +33,42 @@ const RaceTab = ({ gameData }: RaceTabProps) => {
           </Tabs.Trigger>
         ))}
         <Flex gap="2" className="ml-auto">
-          <IconButton variant="soft" className="cursor-pointer" onClick={() => setFilterOpen(true)}>
-            <RiFilter3Line />
-          </IconButton>
+          {view == 'start' && (
+            <IconButton
+              variant="soft"
+              className="cursor-pointer"
+              onClick={() => setFilterOpen(true)}
+            >
+              <RiFilter3Line />
+            </IconButton>
+          )}
           <IconButton variant="soft" className="cursor-pointer" onClick={() => router.push('/')}>
             <RiCalendarLine />
           </IconButton>
         </Flex>
       </Tabs.List>
       <Box pt="3">
+        <SegmentedControl.Root
+          value={view}
+          onValueChange={(value: 'start' | 'statistics' | 'h2h') => setView(value)}
+          className="mb-4 w-full"
+        >
+          <SegmentedControl.Item value="start" className="cursor-pointer">
+            Startlista
+          </SegmentedControl.Item>
+          <SegmentedControl.Item value="statistics" className="cursor-pointer">
+            Statistik
+          </SegmentedControl.Item>
+          <SegmentedControl.Item value="h2h" className="cursor-pointer">
+            Head 2 Head
+          </SegmentedControl.Item>
+        </SegmentedControl.Root>
         {gameData?.races?.map((race, index) => (
           <Tabs.Content key={index} value={race.id}>
             <RaceInfoCard race={race} />
-            <RaceFilterTable game={gameData} race={race} raceIndex={index} />
+            {view === 'start' && <RaceFilterTable game={gameData} race={race} raceIndex={index} />}
+            {view === 'statistics' && <RaceStatistics />}
+            {view === 'h2h' && <H2H />}
           </Tabs.Content>
         ))}
       </Box>

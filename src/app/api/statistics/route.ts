5217d2f0f1880ba992d75493648145609c0b5b76
Flemming <fleeri91@gmail.com>
2025-05-08 02:03:10 +0200

    statistics route

---
+++ src/app/api/statistics/route.ts
@@ -0,0 +1,55 @@
+import { NextResponse, NextRequest } from 'next/server'
+import axios from 'axios'
+
+import { RaceRoot } from '@/types/ATG/Race'
+import { BarlistData } from '@/types/BarlistData'
+import { RaceStatistics } from '@/types/RaceStatistics'
+
+export async function GET(request: NextRequest) {
+  const params = request.nextUrl.searchParams
+  const id = params.get('id')
+
+  if (!id || typeof id !== 'string') {
+    return NextResponse.json({ error: 'Invalid or missing id parameter' }, { status: 400 })
+  }
+
+  try {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/races/${id}`)
+
+    if (!response.data) {
+      return NextResponse.json({ error: 'No data found for the provided race ID' }, { status: 404 })
+    }
+
+    const raceData: RaceRoot = response.data
+
+    const HorseMoneyPerAge: BarlistData[] = raceData.starts.map((start) => {
+      const roundedValue = Math.round(start.horse.money / start.horse.age)
+      return {
+        name: start.horse.name,
+        value: roundedValue,
+      }
+    })
+
+    const HorseWinPercentage: BarlistData[] = raceData.starts.map((start) => {
+      return {
+        name: start.horse.name,
+        value: start.horse.statistics.life.winPercentage,
+      }
+    })
+
+    const raceStatistics: RaceStatistics = {
+      HorseMoneyPerAge,
+      HorseWinPercentage,
+    }
+
+    return NextResponse.json(raceStatistics, { status: 200 })
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return NextResponse.json(
+        { error: error.message || 'Failed to fetch race data' },
+        { status: error.response?.status || 500 }
+      )
+    }
+    return NextResponse.json({ error: 'An unexpected error occurred' }, { status: 500 })
+  }
+}
